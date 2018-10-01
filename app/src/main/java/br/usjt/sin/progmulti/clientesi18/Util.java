51ac86c024135ca4c1c2da75836be9b4cd8d7afc
rafaelonassis <rafael_onassis@hotmail.com>
2018-09-30 23:28:32 -0300

    Entrega

---
+++ app/src/main/java/br/usjt/sin/progmulti/clientesi18/Util.java
@@ -0,0 +1,24 @@
+package br.usjt.sin.progmulti.clientesi18;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import java.lang.reflect.Field;
+
+public class Util {
+
+    public static Drawable getImagem(Context context, String nome){
+        Class<?> c = R.drawable.class;
+        try {
+            Field f = c.getDeclaredField(nome);
+            int id = f.getInt(f);
+            return context.getResources().getDrawable(id, null);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return context.getDrawable(R.drawable.happy);
+    }
+}
